650ce7dd2a61dd23ae23a64e88c70bae8fac83fe
ShunyaYokoe <nkcgta.shunya.yokoe@gmail.com>
2021-09-12 22:38:15 +0900

    ファイル作成

---
+++ Silhouette/Assets/SampleYFile/SsorceY/SamplePlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamplePlayer : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    float Setpos(float pos_x,float pos_y)
+    {
+        return 0;
+    }
+}
--- Silhouette/Assets/Script/Yokoe/Sample_Y.cs
+++ Silhouette/Assets/Script/Yokoe/Sample_Y.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Sample_Y : MonoBehaviour
 {
+    GameObject eye;
+
+    GameObject Player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        eye = GameObject.Find("eye");
+        Player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        eye.transform.position = new Vector3(Mathf.Clamp(eye.transform.position.x, 5, 15), 
+                                                Mathf.Clamp(eye.transform.position.y,1,1));
     }
 }
