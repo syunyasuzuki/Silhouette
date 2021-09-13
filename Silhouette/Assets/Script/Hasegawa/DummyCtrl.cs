b0805fe52c7686d1d1dd9de217fe641a3ca5f733
hasegawaki <hk.lucy.0424@gmail.com>
2021-09-13 09:57:06 +0900

    仮置きの素材を追加 雷の点滅のスクリプトを追加 雷の点滅による障害物と背景のダミーを制御するスクリプトを追加 上の２つのスクリプトを制御するスクリプトを追加

---
+++ Silhouette/Assets/Script/Hasegawa/DummyCtrl.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyCtrl : MonoBehaviour
+{
+    [SerializeField] GameObject DummyObject = null;
+
+    [SerializeField] int CreateNumberOfDummy = 10;
+
+    private struct DummyData
+    {
+        private GameObject dummy;
+        private SpriteRenderer sr;
+
+        public void Create(GameObject d)
+        {
+            dummy = Instantiate(d);
+            dummy.transform.localScale = new Vector3(Random.Range(1.5f, 3.5f), Random.Range(3f, 5f), 1);
+            dummy.transform.position = new Vector3(Random.Range(-7.3f, 7.3f), -4 + dummy.transform.localScale.y / 2f, 0);
+            sr = dummy.GetComponent<SpriteRenderer>();
+        }
+
+        public void Delete()
+        {
+            Destroy(dummy.gameObject);
+            sr = null;
+        }
+
+        public void Color_LightBlack()
+        {
+            //sr.color = new Color(1f / 255 * 33, 1f / 255 * 33, 1f / 255 * 33, 1);
+            sr.color = new Color(0, 0, 0, 1);
+        }
+
+        public void Color_Default()
+        {
+            //sr.color = new Color(1f / 255 * 199, 1f / 255 * 21, 1f / 255 * 133, 1);
+            sr.color = new Color(1f / 255 * 60, 1f / 255 * 60, 1f / 255 * 60, 1);
+        }
+    }
+
+    DummyData[] DummyDataList = null;
+
+    private void CreateDummyObjects()
+    {
+        DummyDataList = new DummyData[CreateNumberOfDummy];
+        for(int i = 0; i < CreateNumberOfDummy; ++i)
+        {
+            DummyDataList[i].Create(DummyObject);
+            DummyDataList[i].Color_LightBlack();
+        }
+
+
+    }
+
+    public void ChangeDefaultColor()
+    {
+        for(int i = 0; i < CreateNumberOfDummy; ++i)
+        {
+            DummyDataList[i].Color_Default();
+        }
+    }
+
+    public void ChangeDummyColor()
+    {
+        for (int i = 0; i < CreateNumberOfDummy; ++i)
+        {
+            DummyDataList[i].Color_LightBlack();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateDummyObjects();
+    }
+
+}
--- Silhouette/Assets/Script/Hasegawa/Sample_H.cs
+++ Silhouette/Assets/Script/Hasegawa/Sample_H.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Sample_H : MonoBehaviour
 {
+    
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
