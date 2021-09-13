3017771d8c74dfbd60015399a9cb85d8dc7e5529
ogihararyuki <nkcgta.ogihara.ryuki@gmail.com>
2021-09-13 09:50:56 +0900

    自分用のファイル作成

---
+++ Silhouette/Assets/Ogihara/Player_test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_test : MonoBehaviour
+{
+    [SerializeField] float Speed;
+    [SerializeField] float Jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void PlayerCon()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerCon();
+    }
+}
