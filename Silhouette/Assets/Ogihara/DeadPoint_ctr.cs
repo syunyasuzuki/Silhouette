e15fcb838afe3d000d8b1566072d75fd7be5162c
ogihararyuki <nkcgta.ogihara.ryuki@gmail.com>
2021-09-15 14:33:44 +0900

    exc_mark追加

--- Silhouette/Assets/Ogihara/DeadPoint_ctr.cs
+++ Silhouette/Assets/Ogihara/DeadPoint_ctr.cs
@@ -6,16 +6,31 @@ using UnityEngine.UI;
 public class DeadPoint_ctr : MonoBehaviour
 {
     bool found_check;
+    [SerializeField] GameObject exc_mark;
+    float alpha;
     // Start is called before the first frame update
     void Start()
     {
         found_check = false;
+        //GameObject.Find("") = GetComponent<FlashCtrl>();
+        alpha = 0;
+        exc_mark.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if(GetFlash() == true && found_check==true)
+        //{
+        //alpha = 1;
+        //Flashctrl.StopFlash();
+        //exc_mark.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, alpha);
+        //}
+        if(found_check == false)
+        {
+            alpha = 1;
+            exc_mark.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -24,14 +39,12 @@ public class DeadPoint_ctr : MonoBehaviour
         {
             found_check = false;
         }
-
     }
 
     void OnTriggerExit2D(Collider2D col)
     {
-
         if (gameObject.tag == "Wall")
-        {
+        {          
             found_check = true;
         }
     }
