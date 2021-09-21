9c1b61cdd56378f190d8e6bff5b3407834faff76
hasegawaki <hk.lucy.0424@gmail.com>
2021-09-21 14:27:31 +0900

    雨を追加 雨の素材を追加 タイトルからゲームへのの遷移を追加 キーを押させるロゴを追加

--- Silhouette/Assets/Script/Hasegawa/PlatformCtrl.cs
+++ Silhouette/Assets/Script/Hasegawa/PlatformCtrl.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//プレイヤーの乗ることのできる足場を光った時だけ見えるようにする
 public class PlatformCtrl : MonoBehaviour
 {
+    //見えるようにする足場
     [SerializeField] SpriteRenderer sr = null;
 
+    //足場を見えるようにする
     public void VisiblePlatform()
     {
         if (sr == null) return;
@@ -13,6 +16,7 @@ public class PlatformCtrl : MonoBehaviour
         sr.color = new Color(1, 1, 1, 1);
     }
 
+    //足場を見えなくする
     public void UnVisiblePlatform()
     {
         if (sr == null) return;
@@ -20,6 +24,7 @@ public class PlatformCtrl : MonoBehaviour
         sr.color = new Color(1, 1, 1, 0);
     }
 
+    //足場のa値を変える
     public void ChangeAlpha(float a)
     {
         if (sr == null) return;
