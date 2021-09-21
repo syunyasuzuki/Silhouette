a94d7a9a33d0d0582e372b27ec1a4230511761be
hasegawaki <hk.lucy.0424@gmail.com>
2021-09-21 14:33:49 +0900

    プレハブを一つのファイルにまとめた

--- Silhouette/Assets/Script/Hasegawa/PlatformCtrl.cs
+++ Silhouette/Assets/Script/Hasegawa/PlatformCtrl.cs
@@ -6,30 +6,30 @@ using UnityEngine;
 public class PlatformCtrl : MonoBehaviour
 {
     //見えるようにする足場
-    [SerializeField] SpriteRenderer sr = null;
+    [SerializeField] SpriteRenderer Platformsr = null;
 
     //足場を見えるようにする
     public void VisiblePlatform()
     {
-        if (sr == null) return;
+        if (Platformsr == null) return;
 
-        sr.color = new Color(1, 1, 1, 1);
+        Platformsr.color = new Color(1, 1, 1, 1);
     }
 
     //足場を見えなくする
     public void UnVisiblePlatform()
     {
-        if (sr == null) return;
+        if (Platformsr == null) return;
 
-        sr.color = new Color(1, 1, 1, 0);
+        Platformsr.color = new Color(1, 1, 1, 0);
     }
 
     //足場のa値を変える
     public void ChangeAlpha(float a)
     {
-        if (sr == null) return;
+        if (Platformsr == null) return;
 
-        sr.color = new Color(1, 1, 1, a);
+        Platformsr.color = new Color(1, 1, 1, a);
     }
 
 
