d5372a7cf1a3bea42b75d0383a7499febe1a8c05
ogihararyuki <nkcgta.ogihara.ryuki@gmail.com>
2021-09-22 10:28:26 +0900

    カメラ修正中一時上げ

--- Silhouette/Assets/Script/Ogihara/Player_Came.cs
+++ Silhouette/Assets/Script/Ogihara/Player_Came.cs
@@ -11,6 +11,8 @@ public class Player_Came : MonoBehaviour
     Transform cameTransform;
     public static bool came_check;
     [SerializeField] Camera came;
+   [SerializeField] float came_move_max =10;
+    [SerializeField] float came_move_min = -10;
 
 
     // Start is called before the first frame update
@@ -35,23 +37,37 @@ public class Player_Came : MonoBehaviour
         //カメラ移動ON/OFF
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            came_check = !came_check;
+            came_check =! came_check;
         }
         switch (came_check)
         {
-            case true:
-                Player_test.move_check = false;
-                Vector3 camepos = cameTransform.position;
+            case true:              
+                    Player_test.move_check = false;
+                    Vector3 camepos = cameTransform.position;
                 if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
                 {
                     camepos.x += 0.1f;
+                    if (camepos.x > came_move_min)
+                    {
+                        camepos.x = 10.0f;
+                    }
                 }
-                if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-                {
-                    camepos.x += -0.1f;
-                }
-                cameTransform.position = camepos;
-            break;
+                if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                    {
+                        camepos.x += -0.1f;
+                    if(camepos.x < came_move_max)
+                    {
+                        camepos.x = -10.0f;
+                    }
+                    }
+                    cameTransform.position = camepos;
+               
+                //if (Input.GetKeyDown(KeyCode.Z))
+                //{                    
+                //    Player_test.move_check = false;
+                //    came_check = false;
+                //}
+                break;
         case false:
                 Player_test.move_check = true;
                 transform.position = new Vector3(playerTransform.position.x, transform.position.y, transform.position.z);
